61174060dc577749644846bc5b3d65ec9ae01a67
brian-hornbrook <brian_hornbrook@outlook.com>
2022-04-28 14:00:12 -0500

    added deploy code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import NoteList from "./NoteList";
+import { notes } from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { notes, title: "", description: "" };
+
+  handleNewTitle = (event) => {
+    const updateState = this.state;
+    updateState.title = event.target.value;
+    this.setState({ updateState });
+  };
+
+  handleNewDescription = (event) => {
+    const updateState = this.state;
+    updateState.description = event.target.value;
+    this.setState({ updateState });
+  };
+
+  addNote = (event) => {
+    event.preventDefault();
+    console.log(`clicked ${this.state.title}`);
+    console.log(`clicked ${this.state.description}`);
+    const updateNotes = {
+      id: Date.now(),
+      title: this.state.title,
+      description: this.state.description,
+      doesMatchSearch: true
+    };
+    this.setState({ notes: [updateNotes, ...this.state.notes] });
+  };
+
+  onType = (id, property, updatedValue) => {
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== id) {
+        return note;
+      } else {
+        if (property === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          if (property === "description") {
+            note.description = updatedValue;
+            return note;
+          }
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  onSearch = (searchText) => {
+    const searchNotes = this.state.notes.map((note) => {
+      if (
+        note.title.includes(searchText) ||
+        note.description.includes(searchText)
+      )
+        note.doesMatchSearch = true;
+      else note.doesMatchSearch = false;
+      return note;
+    });
+    this.setState({ notes: searchNotes });
+  };
+
+  onDelete = (index) => {
+    const updatedNotes = this.state.notes.filter((note) => {
+      if (note.id !== index) return note;
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header
+          onSearch={this.onSearch}
+          addNote={this.addNote}
+          searchText={this.state.searchText}
+          handleNewTitle={this.handleNewTitle}
+          handleNewDescription={this.handleNewDescription}
+        />
+        <NoteList
+          onType={this.onType}
+          notes={this.state.notes}
+          onDelete={this.onDelete}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const notes = [
+  {
+    id: 0,
+    title: "eat",
+    description: "reese peanut butter cups",
+    doesMatchSearch: true
+  },
+  {
+    id: 1,
+    title: "sleep",
+    description: "eight hours",
+    doesMatchSearch: true
+  },
+  {
+    id: 2,
+    title: "code",
+    description: "build an awesome ui",
+    doesMatchSearch: true
+  }
+];
+
+export const search = [
+  {
+    searchText: ""
+  }
+];
