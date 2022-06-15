754a9767f814f001971ec0d12be3dd9e2db19f3d
brian-hornbrook <brian_hornbrook@outlook.com>
2022-06-15 14:56:13 -0500

    Deploy React app to GitHub Pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import "./styles.css";
-
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<App />);
